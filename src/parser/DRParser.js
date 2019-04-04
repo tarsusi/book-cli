f8de085407c7fb536511e481c3e6de9aa040a1ab
Omer Karatas <omer.karatas@commencis.com>
2019-04-05 00:55:15 +0300

    feat: add parse function to DRParser

--- src/parser/DRParser.js
+++ src/parser/DRParser.js
@@ -1,8 +1,43 @@
 // Parser for D&R
+import cheerio from 'cheerio';
+import requestPromise from 'request-promise';
+
+const BOOK_SEARCH_URL = 'https://www.dr.com.tr/search?q=';
 
 class DRParser {
-  parse = (isbn, bookName, author, price) => {
-    console.log(isbn, bookName, author, price); // TODO will be implemented
+  getPageHTML = async (page) => {
+    const pageHTML = await requestPromise(page);
+
+    return pageHTML;
+  };
+
+  parse = async (isbn) => {
+    const pageHTML = await this.getPageHTML(`${BOOK_SEARCH_URL}${isbn}`);
+
+    const $ = cheerio.load(pageHTML);
+
+    const queryBooks = $('#catPageContent .shelf #container .list-cell');
+
+    if (queryBooks.length) {
+      const firstBook = queryBooks.first();
+
+      const bookName = firstBook.find('.details .summary .item-name h3').text();
+      const author = firstBook
+        .find('.details .summary .who')
+        .first()
+        .text();
+      const price = firstBook.find('.details .prices .price').text();
+      const bookImage = firstBook.find('figure .item-name img').attr('src');
+
+      return {
+        bookName,
+        author,
+        price,
+        bookImage,
+      };
+    }
+
+    return {};
   };
 }
 
