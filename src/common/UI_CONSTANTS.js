37058ce98a7370d29844fa9ee951bb2979f39682
Omer Karatas <omer.karatas@commencis.com>
2019-04-04 21:17:44 +0300

    feat: add readAndUpdateFile action

--- src/common/UI_CONSTANTS.js
+++ src/common/UI_CONSTANTS.js
@@ -1,9 +1,15 @@
+import readAndUpdateFile from '../reader/FileReaderUtil';
+
 export const UI_PREFIX = 'book-cli$';
 
 export const UI_COMMANDS = [
   {
     name: 'correct-csv <filePath> [startIndex] [endIndex]',
     explanation: 'Reads a CSV file and fulfill missing data',
-    action: (_, callback) => callback(), // TODO will be implemented later
+    action: (args, callback) => {
+      const { filePath } = args;
+
+      readAndUpdateFile(filePath, callback);
+    },
   },
 ];
