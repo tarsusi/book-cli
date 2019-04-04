4fa3e659aac9d7686cbcd6bebc5a499d057a82d3
Omer Karatas <omer.karatas@commencis.com>
2019-04-04 22:04:45 +0300

    feat:add initial parsers

---
+++ src/parser/DRParser.js
@@ -0,0 +1,9 @@
+// Parser for D&R
+
+class DRParser {
+  parse = (isbn, bookName, author, price) => {
+    console.log(isbn, bookName, author, price); // TODO will be implemented
+  };
+}
+
+export default new DRParser();
---
+++ src/parser/KitapYurduParser.js
@@ -0,0 +1,9 @@
+// Parser for KitapYurdu.com
+
+class KitapYurduParser {
+  parse = (isbn, bookName, author, price) => {
+    console.log(isbn, bookName, author, price); // TODO will be implemented
+  };
+}
+
+export default new KitapYurduParser();
